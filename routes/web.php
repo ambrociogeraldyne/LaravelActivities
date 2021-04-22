f0513df3b8db6e47fa51482e2a15add65daf6e09
ambrociogeraldyne <201810485@gordoncollege.edu.ph>
2021-04-22 19:23:12 +0800

    push update authenticated user

--- routes/web.php
+++ routes/web.php
@@ -22,13 +22,13 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/posts', [PostController::class, 'index'])->middleware('auth.basic');
-Route::get('/posts/create', [PostController::class, 'create'])->middleware('auth.basic');
-Route::post('/posts', [PostController::class, 'store'])->name('posts.store')->middleware('auth.basic');
-Route::get('/posts/{id}', [PostController::class, 'show'])->middleware('auth.basic');
-Route::get('/posts/{id}/edit', [PostController::class, 'edit'])->middleware('auth.basic');
-Route::patch('/posts/{id}', [PostController::class, 'update'])->name('posts.update')->middleware('auth.basic');
-Route::delete('/posts/{id}', [PostController::class, 'destroy'])->name('posts.destroy')->middleware('auth.basic');
+Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts/create', [PostController::class, 'create'])->middleware('auth');
+Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+Route::get('/posts/{id}', [PostController::class, 'show']);
+Route::get('/posts/{id}/edit', [PostController::class, 'edit']);
+Route::patch('/posts/{id}', [PostController::class, 'update'])->name('posts.update')->middleware('auth');
+Route::delete('/posts/{id}', [PostController::class, 'destroy'])->name('posts.destroy')->middleware('auth');;
 
 //Auth::routes();
 
